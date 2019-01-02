2d5ff814051fc43dc0354621168d693ea8c3dfb2
wpx <wangpx@minstone.com.cn>
2019-01-02 15:22:31 +0800

    修改请求封装

--- src/assets/js/require.js
+++ src/assets/js/require.js
@@ -5,17 +5,18 @@
 import axios from 'axios';
 
 /**
+ * 通用请求方法
  * @param {请求路径：''} url 
  * @param {请求方式：post、get} method 
  * @param {请求参数：{}} data 
  * @param {成功回调：function} successCallBack 
  * @param {失败回调：funciton} failCallBack 
  */
-function commonAxios (url, method, data, successCallBack, failCallBack) {
+function commonAxios (url = '', method = 'POST', data = {}, successCallBack, failCallBack) {
   axios({
-    method: method || 'POST',
-    url: url || '',
-    params: data || {}
+    method: method,
+    url: url,
+    params: data
   }).then((res) => {
     successCallBack && successCallBack(res);
   }).catch((err) => {
